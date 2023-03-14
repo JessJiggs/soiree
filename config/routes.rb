d9ad3688a9529881ebd7137e34268b2fa1bf4415
Yonela Johannes <johannesyonela@gmail.com>
2023-03-14 15:36:49 +0200

    feat: User is able to create event

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :events, only: [ :index, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
