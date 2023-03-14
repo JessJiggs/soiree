f319e7b772bb1bd6cfcdb3fc6f8a63fa18f9c332
MoJafa <jmohsa@gmail.com>
2023-03-14 15:53:40 +0200

    established relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   has_many :collaborations, dependent: :destroy
   has_many :expenses, dependent: :destroy
   has_many :tasks, dependent: :destroy
+  has_many :guests, dependent: :destroy
 end
