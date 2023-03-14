dddc2e89c3efc10ee1cee0b28ce4a96b6c2f8d37
MoJafa <jmohsa@gmail.com>
2023-03-14 15:26:13 +0200

    fixed schema and migration issues on changing role from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_131534) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_131747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_131534) do
 
   create_table "collaborations", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "role"
+    t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "event_id", null: false
