48588eff92fe33ef94323d9fbcf6932dd47e444d
Abigail Goschen <123146410+agoschen@users.noreply.github.com>
2023-03-15 17:08:01 +0200

    added default 0 to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_122437) do
     t.string "name"
     t.text "description"
     t.datetime "due_date"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_tasks_on_event_id"
