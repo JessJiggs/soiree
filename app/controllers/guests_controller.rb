41001c64da18f6ec7941249df6d622e2e4b7c403
JessJiggs <verheuljess@gmail.com>
2023-03-20 09:30:17 +0200

    added update method to controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -16,6 +16,17 @@ class GuestsController < ApplicationController
     end
   end
 
+  def update
+    @event = Event.find(params[:event_id])
+    guest = Guest.find(guest[:id])
+    guest.update!(guest_params)
+    if @guest.save
+      redirect_to event_guests_path(@event), notice: "Guest was successfully updated"
+    else
+      redirect_to event_guests_path(@event), status: :unprocessable_entity, notice: "Guest was not successfully updated."
+    end
+  end
+
   private
   def guest_params
     params.require(:guest).permit(:first_name, :last_name, :email_address, :phone_number)
