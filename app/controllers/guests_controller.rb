364e985b1c4c8dcaa1bb81f8dd0e29ad69494bd7
JessJiggs <verheuljess@gmail.com>
2023-03-20 13:30:20 +0200

    edit a guest works and saves without reload

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,7 @@
 class GuestsController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
-    @guests = @event.guests
+    @guests = @event.guests.order(updated_at: :desc)
     @guest = Guest.new
   end
 
