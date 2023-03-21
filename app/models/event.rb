d2a29b1e385a15fc845a96b62aaff2bd59f03bb0
MoJafa <jmohsa@gmail.com>
2023-03-21 12:19:03 +0200

    added labels and validations  to event create form

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,15 @@ class Event < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :guests, dependent: :destroy
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :venue_name, presence: true
+  validates :venue_address, presence: true
+  validates :est_guests, presence: true
+  validates :total_budget, presence: true
+
   include PgSearch::Model
   pg_search_scope :search_by_name,
     against: [ :name ],
