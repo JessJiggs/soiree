76e08bcce32ccd6e0df420736c2d739c652c481e
JessJiggs <verheuljess@gmail.com>
2023-03-21 11:07:16 +0200

    edit to controller -format code and updated the edit functionality

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,10 @@
 class ExpensesController < ApplicationController
   def index
-    @expense = Expense.new
     @event = Event.find(params[:event_id])
+    @expense = Expense.new
     @expense.event = @event
-    @expenses = @event.expenses
+    # do we need this ^^^ if we already on the expense index (tasks doesnt have find and guests doesnt have this ^? (assign this event to this expense) -delete comment when done
+    @expenses = @event.expenses.order(updated_at: :desc)
 
     categories = ["Catering", "Bar", "Entertainment", "Decor", "Venue", "Services", "Transport", "Other"]
 
@@ -13,41 +14,43 @@ class ExpensesController < ApplicationController
     end
   end
 
-  def show
-    @event = Event.find(params[:event_id])
-    @expenses = @event.set_expense
-  end
+  # def show
+  #   @event = Event.find(params[:event_id])
+  #   @expenses = @event.set_expense
+  # end
 
   def create
-    @expense = Expense.new(expense_params)
-    # an expense belongs to an event
     @event = Event.find(params[:event_id])
+    @expense = Expense.new(expense_params)
     @expense.event = @event
     if @expense.save
-      redirect_to event_expenses_path(@event)
+      redirect_to event_expenses_path(@event), notice: "Expense was successfully added."
     else
-      render :new
+      # render :new
+      redirect_to event_expenses_path(@event), status: :unprocessable_entity, notice: "Expense was not successfully added."
     end
   end
 
-  def edit
-    set_expense
-  end
+  # def edit
+  #   set_expense
+  # end
 
   def update
-    set_expense
-    if @expense.update(expense_params)
-      redirect_to @expense
+    @event = Event.find(params[:event_id])
+    expense = Expense.find(params[:id])
+    expense.update!(expense_params)
+    if expense.save
+      redirect_to event_expenses_path(@event), notice: "Expense was successfully updated."
     else
-      render :edit
+      redirect_to event_expenses_path(@event), status: :unprocessable_entity, notice: "Expense was not successfully updated."
     end
   end
 
   private
 
-  def set_expense
-    @expense = Expense.find(params[:id])
-  end
+  # def set_expense
+  #   @expense = Expense.find(params[:id])
+  # end
 
   def expense_params
     params.require(:expense).permit(:name, :amount_spent, :status, :category_list)
