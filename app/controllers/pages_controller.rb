a5b8d869e7dfbe7e71303af6595a7944636f89ed
Abigail Goschen <123146410+agoschen@users.noreply.github.com>
2023-03-21 16:37:35 +0200

    tasks pulling onto dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,13 @@ class PagesController < ApplicationController
     if current_user
       if params[:query].present?
         @events = current_user.events.search_by_name(params[:query]).order(updated_at: :desc)
+        this_weeks_tasks = Task.where(due_date: (Date.today..Date.today + 7))
+        @grouped_tasks = this_weeks_tasks.group_by { |task| task.due_date.strftime("%A") }
+
       else
         @events = current_user.events
+        this_weeks_tasks = Task.where(due_date: (Date.today..Date.today + 7))
+        @grouped_tasks = this_weeks_tasks.group_by { |task| task.due_date.strftime("%A") }
       end
     else
       render "pages/landing_page"
