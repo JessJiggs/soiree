2c2e34983d4555be7844e04d9e27296ac245da84
MoJafa <jmohsa@gmail.com>
2023-03-21 11:08:03 +0200

    added search feature on guests page

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,10 @@ class Expense < ApplicationRecord
   belongs_to :event
   enum :status, [:paid, :unpaid]
   acts_as_taggable_on :categories
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_amount_spent_and_status,
+    against: %i[name amount_spent status],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
