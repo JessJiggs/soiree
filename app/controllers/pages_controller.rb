cd0f87112e18a92bb74eaa39b16c6c7a85cebf99
Abigail Goschen <123146410+agoschen@users.noreply.github.com>
2023-03-23 11:47:37 +0200

    tasks on dashboard by user and ordered by date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,20 @@ class PagesController < ApplicationController
     if current_user
       @notes = current_user.notes
       @note = Note.new
+      collaborations = current_user.collaborations
+      tasks = collaborations.map do | collaboration |
+        collaboration.tasks
+      end
+      this_weeks_tasks = tasks.flatten.select { |task| task.due_date >= Date.today && task.due_date <= Date.today + 7 }
+      this_weeks_tasks = this_weeks_tasks.sort_by { |task| task.due_date }
+
+      @grouped_tasks = this_weeks_tasks.group_by { |task| task.due_date.strftime("%A") }
+      @grouped_tasks.sort_by
+
       if params[:query].present?
         @events = current_user.events.search_by_name(params[:query]).order(updated_at: :desc)
-        this_weeks_tasks = Task.where(due_date: (Date.today..Date.today + 7))
-        @grouped_tasks = this_weeks_tasks.group_by { |task| task.due_date.strftime("%A") }
-
       else
         @events = current_user.events
-        this_weeks_tasks = Task.where(due_date: (Date.today..Date.today + 7))
-        @grouped_tasks = this_weeks_tasks.group_by { |task| task.due_date.strftime("%A") }
       end
     else
       render "pages/landing_page"
