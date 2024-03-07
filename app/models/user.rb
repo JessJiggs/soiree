79080e9d536ec64608be2914dfc0bbf1c8231b7e
JessJiggs <verheuljess@gmail.com>
2024-03-07 15:43:28 +0200

    user can now signup with name and last name - that their name displays on dash and its not Nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
   has_many :collaborations, dependent: :destroy
   has_many :assignments, through: :collaborations
   has_many :tasks, through: :assignments
